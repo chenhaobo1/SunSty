5dc5cbd6d0ff867b73b5de5cd217623d2f5e1585
chenhaobo1 <3517730047@qq.com>
2021-04-13 23:33:40 +0800

    创建axios到原型

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import http from './http'
 
 Vue.use(ElementUI)
+Vue.prototype.$http = http
 
 Vue.config.productionTip = false
 
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+import Categories from '../views/CategoriesEidt'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'main',
+    component: Main,
+    children:[
+      {path:'/categories/create',component:Categories}
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
